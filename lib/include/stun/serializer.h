7f0844c62df3d8dc64a2c0efc0c18191b6e6ed7f
Artemenko Dmitriy <dvartemenkowork@gmail.com>
2022-09-11 23:19:51 +0300

    Remove useless interface

--- lib/include/stun/serializer.h
+++ lib/include/stun/serializer.h
@@ -3,30 +3,67 @@
 
 #include <array>
 #include <memory>
+#include <vector>
+#include <boost/endian/conversion.hpp>
 
-#include "stun/interface/iserializer.h"
-#include "stun/utils/fast_pimpl.h"
+#include "stun/utils/traits.h"
 
 namespace stun {
 
-class Serializer final : public ISerializer {
+class Serializer final {
  public:
-  Serializer();
-  ~Serializer() noexcept override;
+  std::vector<uint8_t> Data() const;
+  void AlignData(size_t align);
 
-  std::vector<uint8_t> Data() const override;
-  void AlignData(size_t align) override;
+  template <class T, std::enable_if_t<std::is_pod_v<T>, bool> = true>
+  Serializer& Serialize(const T& value) {
+    CheckSize(sizeof(value));
 
-  ISerializer& Serialize(char value) override;
-  ISerializer& Serialize(uint8_t value) override;
-  ISerializer& Serialize(uint16_t value) override;
-  ISerializer& Serialize(uint32_t value) override;
-  ISerializer& Serialize(uint64_t value) override;
+    new (NextPos()) T{boost::endian::native_to_big(value)};
+    size_ += sizeof(value);
+    return *this;
+  }
+
+  template <class Container,
+            std::enable_if_t<!std::is_pod_v<Container>, bool> = true,
+            std::enable_if_t<utils::is_iterable_v<Container>, bool> = true>
+  Serializer& Serialize(const Container& container) {
+    using Value = typename Container::value_type;
+
+    for (Value val : container) {
+      this->Serialize(val);
+    }
+    return *this;
+  }
+
+  template <class Object, std::enable_if_t<!std::is_pod_v<Object>, bool> = true,
+            std::enable_if_t<!utils::is_iterable_v<Object>, bool> = true>
+  Serializer& Serialize(const Object& obj) {
+    obj.Serialize(*this);
+    return *this;
+  }
 
  private:
-  class Impl;
-  utils::FastPimpl<Impl, 1024, 8> impl_;
+  uint8_t* NextPos();
+  void CheckSize(size_t s) const;
+
+  std::array<uint8_t, 1016> data_;
+  size_t size_;
 };
+
+//------------------------------------------------------------------------------
+
+template <class T>
+Serializer& operator&(Serializer& s, const T& value) {
+  return s.Serialize(value);
+}
+
+//------------------------------------------------------------------------------
+
+template <class T>
+Serializer& operator&(Serializer& s, std::shared_ptr<T> value) {
+  return s.Serialize(*value);
+}
 
 }  // namespace stun
 
